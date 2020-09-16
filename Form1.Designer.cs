93de7fac990ef8519b82679e6a8b91b4de73b11a
Vadim Guseynov <hgt166782@gmail.com>
2020-09-16 19:53:55 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace jpview
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.pdfViewer = new PdfiumViewer.PdfViewer();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.buttonTop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pdfViewer
+            // 
+            this.pdfViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pdfViewer.Location = new System.Drawing.Point(0, 0);
+            this.pdfViewer.Name = "pdfViewer";
+            this.pdfViewer.Size = new System.Drawing.Size(491, 371);
+            this.pdfViewer.TabIndex = 0;
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAbout.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAbout.Location = new System.Drawing.Point(446, 0);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(45, 20);
+            this.buttonAbout.TabIndex = 1;
+            this.buttonAbout.Text = "About";
+            this.buttonAbout.UseVisualStyleBackColor = true;
+            this.buttonAbout.Click += new System.EventHandler(this.ButtonAbout_Click);
+            // 
+            // buttonTop
+            // 
+            this.buttonTop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonTop.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonTop.Location = new System.Drawing.Point(388, 0);
+            this.buttonTop.Name = "buttonTop";
+            this.buttonTop.Size = new System.Drawing.Size(52, 20);
+            this.buttonTop.TabIndex = 2;
+            this.buttonTop.Text = "Top";
+            this.buttonTop.UseVisualStyleBackColor = true;
+            this.buttonTop.Click += new System.EventHandler(this.ButtonTop_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 371);
+            this.Controls.Add(this.buttonTop);
+            this.Controls.Add(this.buttonAbout);
+            this.Controls.Add(this.pdfViewer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "JpViewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PdfiumViewer.PdfViewer pdfViewer;
+        private System.Windows.Forms.Button buttonAbout;
+        private System.Windows.Forms.Button buttonTop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using PdfiumViewer;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace jpview
+{
+    public partial class FormMain : Form
+    {
+        private string icao;
+        private WebClient web = new WebClient();
+
+        public FormMain(string[] args)
+        {
+            InitializeComponent();
+            web.DownloadFileCompleted += new AsyncCompletedEventHandler(OnFileDownloadCompleted);
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Enter ICAO as first argument");
+                Exit();
+            }
+
+            icao = args[0].ToUpper();
+
+            if (!ValidateIcao(icao))
+            {
+                Console.WriteLine("Unknown ICAO");
+                Exit();
+            }
+
+            new Thread(() =>
+            {
+                string fileName = icao + ".pdf";
+                if (!File.Exists(fileName)) {
+                    web.DownloadFileAsync(new Uri(string.Format("https://vau.aero/navdb/chart/{0}.pdf", icao)), fileName, fileName);
+                }
+                else
+                {
+                    SetPdf(fileName);
+                }
+            }).Start();
+        }
+
+        private void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        private void OnFileDownloadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            SetPdf(e.UserState.ToString());
+        }
+
+        private void SetPdf(string fileName)
+        {
+            var pdf = PdfDocument.Load(fileName);
+            pdfViewer.Invoke(new Action(() => pdfViewer.Document = pdf));
+        }
+
+        private bool ValidateIcao(string icao)
+        {
+            if (icao.Length != 4)
+            {
+                return false;
+            }
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://airportsbase.org/search.php?code=" + icao);
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                return response.StatusCode == HttpStatusCode.OK;
+            } catch (WebException)
+            {
+                return false;
+            }
+        }
+
+        private void ButtonAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("Created by DeMmAge\nVersion - {0}\nUsed resources:\n" +
+                "https://vau.aero/\n" +
+                "http://airportsbase.org/", typeof(Program).Assembly.GetName().Version.ToString()), "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ButtonTop_Click(object sender, EventArgs e)
+        {
+            if (TopMost)
+            {
+                TopMost = false;
+                buttonTop.Text = "Top";
+            } else
+            {
+                TopMost = true;
+                buttonTop.Text = "UnTop";
+            }
+        }
+    }
+}
